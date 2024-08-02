f4c5faec1626bfad9d040ba29ef7f1d13ce259f1
Basel-Dawoud <134341247+Basel-Dawoud@users.noreply.github.com>
2024-08-03 00:53:41 +0300

    main.c

--- eShellTask/main.c
+++ eShellTask/main.c
@@ -1,134 +1,170 @@
+#include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <fcntl.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <limits.h>
 
-#include "commands.h"
 
-#define STDIN 0
-#define STDOUT 1
-#define STDERR 2
-#define MAX_READB 1024
+#define MAX_BUFFER_SIZE 1024
 
-extern char **environ;  // Declaration of environ
+// Print all environment variables (same as env)
+void envir(void) {
+    extern char **environ;
+    char **env = environ;
+    while (*env) {
+        printf("%s\n", *env++);
+    }
+}
+
+// Printing The Current Working Directory
+void currDir(void) {
+    size_t size = MAX_BUFFER_SIZE;
+    char bufarr[MAX_BUFFER_SIZE];
+    char *buf = bufarr;
+    char *dir = getcwd(buf, size);
+
+    if (dir != NULL) {
+        printf("The current working directory is: %s\n", buf);
+    } else {
+        perror("Error getting current working directory");
+    }
+}
+
+// Printing the input string
+void Echo(void) {
+    char *token;
+
+    // Tokenize the input string
+    token = strtok(NULL, " ");
+
+    // Walk through tokens
+    while (token != NULL) {
+        printf(" %s", token);
+        token = strtok(NULL, " ");
+    }
+    printf("\n");
+}
+
+// Copying a file to another
+void cop(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: cop <sourcePath> <targetPath> [-a]\n");
+        return;
+    }
+
+    const char *srcPath = argv[1];
+    const char *destPath = argv[2];
+    int append = 0;
+
+    if (argc == 4 && strcmp(argv[1], "-a") == 0) {
+        append = 1;
+        srcPath = argv[2];
+        destPath = argv[3];
+    }
+
+    int srcfd = open(srcPath, O_RDONLY);
+    if (srcfd == -1) {
+        perror("Error opening source file");
+        return;
+    }
+
+    int flags = O_WRONLY | O_CREAT | (append ? O_APPEND : O_TRUNC);
+    int destfd = open(destPath, flags, 0644);
+    if (destfd == -1) {
+        perror("Error opening destination file");
+        close(srcfd);
+        return;
+    }
+
+    char buff[MAX_BUFFER_SIZE];
+    ssize_t bytesRead;
+    while ((bytesRead = read(srcfd, buff, sizeof(buff))) > 0) {
+        if (write(destfd, buff, bytesRead) != bytesRead) {
+            perror("Error writing to destination file");
+            close(srcfd);
+            close(destfd);
+            return;
+        }
+    }
+
+    if (bytesRead == -1) {
+        perror("Error reading from source file");
+    }
+
+    close(srcfd);
+    close(destfd);
+}
+
+// Moving a file to another place
+void MV(int argc, char **argv) {
+    if (argc < 3) {
+        fprintf(stderr, "Usage: MV <sourcePath> <targetPath>\n");
+        return;
+    }
+
+    const char *srcPath = argv[1];
+    const char *destPath = argv[2];
+
+    if (rename(srcPath, destPath) == -1) {
+        perror("Error moving file");
+    }
+}
+
+// Print help information
+void Help(void) {
+    printf("Supported commands are:\n");
+    printf("1- currDir : Print the current working directory\n");
+    printf("2- Echo <string> : Print the user input string on stdout\n");
+    printf("3- cop <sourcePath> <destinationPath> [-a] : Copy a file to another file\n");
+    printf("   Use -a option to append to the target file\n");
+    printf("4- MV <sourcePath> <destinationPath> : Move a file to another place\n");
+    printf("5- mycd <path> : Change the current working directory to the specified path\n");
+    printf("6- Exit : Print 'Good Bye' and terminate the shell\n");
+    printf("7- Help : Print this help message\n");
+}
+
+// Exit the shell
+void Exit(void) {
+    printf("Good Bye :)\n");
+    exit(0);
+}
+
+
+// Determine if the command is internal
+int isBuiltinCommand(const char *command) {
+    const char *builtinCommands[] = {"currDir", "Echo", "cop", "MV", "Help", "Exit", "envir", "mycd", "type"};
+    for (int i = 0; i < sizeof(builtinCommands) / sizeof(builtinCommands[0]); i++) {
+        if (strcmp(command, builtinCommands[i]) == 0) {
+            return 1; // True, it's a built-in command
+        }
+    }
+    return 0; // False, it's not a built-in command
+}
+
+// Changing the directory to the target path
+int mycd(const char *path) {
+    if (chdir(path) == -1) {
+        perror("chdir");
+        return -1;
+    }
+    return 0;
+}
+
 
 // Function to execute an external command
 void executeExternalCommand(const char *command, char **args) {
+    /*Debugging output
+    printf("Executing command: %s\n", command);
+    if (args[0] != NULL) {
+        printf("Arguments:\n");
+        for (int i = 0; args[i] != NULL; ++i) {
+            printf("  args[%d] = %s\n", i, args[i]);
+        }
+    }*/
     if (execvp(command, args) == -1) {
         perror("execvp");
         exit(EXIT_FAILURE);  // Exit with failure status
     }
 }
-
-int main(int argc, char **argv) {
-    char commandBuff[MAX_READB];
-    char *token;
-    ssize_t readSize = 0;
-    const char *shellMsg = "5air> ";
-
-    while (1) {
-        /* Display prompt message */
-        if (write(STDOUT, shellMsg, strlen(shellMsg)) == -1) {
-            perror("write");
-            return 1;
-        }
-
-        /* Read input from standard input */
-        readSize = read(STDIN, commandBuff, MAX_READB);
-        if (readSize == -1) {
-            perror("read");
-            return 1;
-        }
-        if (readSize == 0) {
-            // End of file (EOF) or no input; continue to the next iteration
-            continue;
-        }
-
-        /* Null-terminate the buffer */
-        commandBuff[readSize] = '\0';
-
-        /* Get the first token "Command" */
-        token = strtok(commandBuff, " \n");
-        if (token != NULL) {
-            if (isBuiltinCommand(token)) {
-                // Handle built-in commands
-                if (strcmp(token, "currDir") == 0) {
-                    currDir();
-                } else if (strcmp(token, "Echo") == 0) {
-                    Echo();
-                } else if (strcmp(token, "cop") == 0) {
-                    char *args[4];
-                    int i = 0;
-                    args[i++] = token;
-                    while ((token = strtok(NULL, " \n")) != NULL && i < 4) {
-                        args[i++] = token;
-                    }
-                    args[i] = NULL;  // Null-terminate the argument list
-                    cop(i, args);
-                } else if (strcmp(token, "MV") == 0) {
-                    char *args[3];
-                    int i = 0;
-                    args[i++] = token;
-                    while ((token = strtok(NULL, " \n")) != NULL && i < 3) {
-                        args[i++] = token;
-                    }
-                    args[i] = NULL;  // Null-terminate the argument list
-                    MV(i, args);
-                } else if (strcmp(token, "Help") == 0) {
-                    Help();
-                } else if (strcmp(token, "Exit") == 0) {
-                    Exit();
-                } else if (strcmp(token, "envir") == 0) {
-                    envir();
-                } else if (strcmp(token, "mycd") == 0) {
-                    char *args[2];
-                    int i = 0;
-                    args[i++] = token;
-                    while ((token = strtok(NULL, " \n")) != NULL && i < 2) {
-                        args[i++] = token;
-                    }
-                    args[i] = NULL;  // Null-terminate the argument list
-                    if (i > 1) {
-                        mycd(args[1]);
-                    } else {
-                        fprintf(stderr, "Usage: mycd <path>\n");
-                    }
-                }
-            } else {
-                // External commands
-                pid_t pid = fork();
-                if (pid == -1) {
-                    perror("Failed to fork!");
-                } else if (pid == 0) {  // Child process
-                    // Dynamic allocation for arguments
-                    char *args[MAX_READB / 2];
-                    int i = 0;
-                    args[i++] = token;
-                    
-                    while ((token = strtok(NULL, " \n")) != NULL) {
-                        if (i >= (MAX_READB / 2 - 1)) {
-                            fprintf(stderr, "Too many arguments\n");
-                            exit(EXIT_FAILURE);
-                        }
-                        args[i++] = token;
-                    }
-                    args[i] = NULL;  // Null-terminate the argument list
-
-                    executeExternalCommand(args[0], args);
-                } else {  // Parent process
-                    int wstatus;
-                    if (waitpid(pid, &wstatus, 0) == -1) {
-                        perror("waitpid");
-                    }
-                }
-            }
-        }
-    }
-
-    return 0;
-}
-
